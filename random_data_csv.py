5c31ae6327e0bfecd6cfcf5560b859c9a88830a6
Ondrej Martinek <martinek.ondrej.cz@gmail.com>
2026-03-23 15:00:18 +0100

    index improvment

--- random_data_csv.py
+++ random_data_csv.py
@@ -36,7 +36,7 @@ def random_signup_date():
 
 
 rows = []
-for i in range(1, int(number_of_records)):
+for i in range(1, int(number_of_records) + 1):
     age = random.randint(18, 99)
     country = random.choice(countries)
     plan = random.choice(plans)
